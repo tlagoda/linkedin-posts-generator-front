1bea231c420f69e18e91444ac1f9628ad0dbc091
tlagoda <thibautlagoda@hotmail.fr>
2023-06-04 09:40:22 +0200

    set up .env

--- src/components/ConnectButton.vue
+++ src/components/ConnectButton.vue
@@ -3,19 +3,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const loginToLinkedIn = ref(() => {
-//   const clientId = process.env.VUE_APP_LINKEDIN_CLIENT_ID;
-//   const redirectUri = encodeURIComponent('http://localhost:8080/linkedin-callback');
-//   const state = 'randomString';
-//   const scope = encodeURIComponent('r_liteprofile r_emailaddress w_member_social');
-//   const responseType = 'code';
+  const clientId = import.meta.env.VUE_APP_LINKEDIN_CLIENT_ID
+  const redirectUri = encodeURIComponent('http://localhost:8080/linkedin-callback')
+  const state = 'randomString'
+  const scope = encodeURIComponent('r_liteprofile r_emailaddress w_member_social')
+  const responseType = 'code'
 
-//   const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+  const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`
 
-  window.location.href = 'http://google.com'; // replace with linkedin redirect url
-});
+  window.location.href = 'http://google.com' // replace with linkedin redirect url
+})
 </script>
 
 <style>
