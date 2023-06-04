2c57c8c88497019a5e15e17b62307e07e4665fae
tlagoda <thibautlagoda@hotmail.fr>
2023-06-04 11:51:31 +0200

    style MyHome

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,12 +1,44 @@
 <template>
-  <h1>HOME</h1>
-  <button>Generate a post</button>
-  <LinkedInPost />
-  <button>Share on LinkedIn</button>
+  <h1>LINKEDIN / GPT</h1>
+  <div class="post-div">
+    <LinkedInPost />
+    <div class="btn-div">
+      <button>Generate a post</button>
+      <button>Share on LinkedIn</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import LinkedInPost from '@/components/LinkedInPost.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.post-div {
+  width: 50vw;
+  min-width: 20rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.btn-div {
+  margin-top: 1rem;
+  display: flex;
+  width: 20rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+button {
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.2rem 0.8rem;
+}
+</style>
