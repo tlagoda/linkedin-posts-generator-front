8639bf851c2394f47a9f98d0e39bf8ddec330377
tlagoda <thibautlagoda@hotmail.fr>
2023-06-04 09:20:33 +0200

    add connect button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <router-view></router-view>
+  <Connect-Button />
 </template>
 
 <script setup lang="ts">
+import ConnectButton from './components/ConnectButton.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/ConnectButton.vue
@@ -0,0 +1,16 @@
+<template>
+   <button class="connect-btn">Login to LinkedIn</button>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .connect-btn {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        cursor: pointer;
+    }
+</style>
